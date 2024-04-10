73eba15648f4abe16c204bb6e71d5e78da17e874
Enzo Arjona <arjonaenzo1999.@gmail.com>
2024-04-09 23:01:22 -0300

    responsive progress

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,23 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { motion } from "framer-motion";
-import { Pacifico } from "next/font/google";
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { Pacifico } from 'next/font/google';
 
 const pasifico = Pacifico({
-  subsets: ["latin"],
-  weight: "400",
+  subsets: ['latin'],
+  weight: '400',
 });
 
 export default function Home() {
   const bgAnimate = {
     hidden: {
-      clipPath: "polygon(21% 26%, 77% 26%, 77% 77%, 21% 77%)",
+      clipPath: 'polygon(21% 26%, 77% 26%, 77% 77%, 21% 77%)',
     },
     show: {
-      clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+      clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
       transition: {
-        ease: "easeInOut",
+        ease: 'easeInOut',
         duration: 0.8,
         delay: 1,
       },
@@ -25,14 +25,14 @@ export default function Home() {
   };
   const textAnimate1 = {
     hidden: {
-      y: "100%",
+      y: '100%',
       opacity: 0,
     },
     show: {
       y: 0,
       opacity: 1,
       transition: {
-        ease: "easeInOut",
+        ease: 'easeInOut',
         duration: 0.8,
         staggerChildren: 0.4,
         delayChildren: 1,
@@ -46,7 +46,7 @@ export default function Home() {
     show: (i) => ({
       x: i,
       transition: {
-        ease: "easeOut",
+        ease: 'easeOut',
         duration: 0.8,
       },
     }),
@@ -70,18 +70,18 @@ export default function Home() {
       opacity: 1,
       x: 0,
       transition: {
-        ease: "easeInOut",
+        ease: 'easeInOut',
       },
     },
   };
   const navAnimate = {
     hidden: {
-      y: "-110%",
+      y: '-110%',
     },
     show: {
       y: 0,
       transition: {
-        type: "spring",
+        type: 'spring',
         stiffness: 60,
         delay: 2,
       },
@@ -89,70 +89,57 @@ export default function Home() {
   };
   const textParagraph = {
     hidden: {
-      y: "-100%",
+      y: '-100%',
       opacity: 0,
     },
     show: {
       y: 0,
       opacity: 1,
       transition: {
-        type: "spring",
+        type: 'spring',
         stiffness: 60,
         delay: 2.8,
       },
     },
   };
   return (
-    <main className="h-screen px-4 overflow-hidden">
-      <motion.div
-        className="absolute inset-0 w-screen h-screen z-0"
-        variants={bgAnimate}
-        initial="hidden"
-        animate="show"
-      >
-        <Image
-          src="/img/LOGO-NEGRO-FONDO-BLANCO.jpg"
-          alt="background"
-          fill
-          sizes="(max-width:768px) 33vw, (max-width:1024px) 50vw, 100vw"
-          priority={true}
-          className="object-cover brightness-50"
-        />
-      </motion.div>
-
+    <motion.main
+      className=" w-screen h-screen flex flex-col items-center  bg-cover bg-no-repeat bg-center  "
+      style={{
+        backgroundImage: 'url("/img/LOGO-NEGRO-FONDO-BLANCO.jpg")',
+      }}
+    >
       <motion.nav
-        className="flex justify-between items-center text-white relative z-10 pt-4"
+        className="flex justify-between items-center w-full text-black relative z-10 pt-4 px-2  "
         variants={navAnimate}
         initial="hidden"
         animate="show"
       >
-        <div
-          className={`text-xl text-white font-bold underline ${pasifico.className}`}
-        >
+        <div className={`text-lg font-bold underline ${pasifico.className}  `}>
           ATACAMA
         </div>
-        <ul className="w-[300px] flex justify-between items-center">
+        <ul className="w-[70%] sm:w-[50%] flex gap-2 items-center  justify-between ">
           <li>Home</li>
           <li>Sobre Nosotros</li>
           <li>Contacto</li>
         </ul>
       </motion.nav>
-      <div className="relative top-[120px]">
+      <div className="relative top-[120px] bg-red-700">
         <motion.div
-          className="relative left-[25%]"
+          className="relative left-[25%] bg-blue-700"
           variants={textAnimate1}
           initial="hidden"
           animate="show"
         >
           <motion.h1
-            className={`text-9xl text-[#eaeaea] lowercase tracking-tighter font-semibold z-10 ${pasifico.className}`}
+            className={`text-7xl text-black lowercase tracking-tighter font-semibold z-10 ${pasifico.className}`}
             variants={textAnimate2}
             custom={-150}
           >
             ESTUDIO
           </motion.h1>
         </motion.div>
-        <motion.p
+        {/* <motion.p
           className="absolute top-12 right-32 z-10 w-[500px] text-justify leading-5 text-[#eaeaea] text-xs font-medium"
           variants={textParagraph}
           initial="hidden"
@@ -163,11 +150,11 @@ export default function Home() {
             creativo. Somos un estudio audiovisual especializado en eventos
             sociales, comprometidos en transformar cada ocasión en una
             experiencia visualmente impactante y emocionalmente auténtica.
-          </span>{" "}
+          </span>{' '}
           Confía en nosotros para capturar tus momentos más preciados y
           convertirlos en recuerdos que perduren toda la vida.
-        </motion.p>
-        <motion.div
+        </motion.p> */}
+        {/* <motion.div
           className="relative left-[20%]"
           variants={textAnimate1}
           initial="hidden"
@@ -180,51 +167,53 @@ export default function Home() {
           >
             AUDIOVISUAL
           </motion.h1>
-        </motion.div>
+        </motion.div> */}
       </div>
-      <motion.div
-        className="flex gap-4 absolute bottom-4"
-        variants={imageAnimate}
-        initial="hidden"
-        animate="show"
-      >
+      {/* <div className="flex gap-3 flex-wrap">
         <motion.div
-          className="relative w-[300px] h-[200px]"
-          variants={imageAnimateChild}
+          className="flex gap-4 absolute bottom-4"
+          variants={imageAnimate}
+          initial="hidden"
+          animate="show"
         >
-          <Image
-            src="/img/GCV9NdDWgAA_H1v.jpg"
-            alt="image"
-            fill
-            sizes="(max-width:768px) 33vw, (max-width:1024px) 50vw, 100vw"
-            className="object-cover rounded-sm saturate-150"
-          />
+          <motion.div
+            className="relative w-[300px] h-[200px]"
+            variants={imageAnimateChild}
+          >
+            <Image
+              src="/img/GCV9NdDWgAA_H1v.jpg"
+              alt="image"
+              fill
+              sizes="(max-width:768px) 33vw, (max-width:1024px) 50vw, 100vw"
+              className="object-cover rounded-sm saturate-150"
+            />
+          </motion.div>
+          <motion.div
+            className="relative w-[300px] h-[200px]"
+            variants={imageAnimateChild}
+          >
+            <Image
+              src="/img/GDusokhWQAAVJOd.jpg"
+              alt="image"
+              fill
+              sizes="(max-width:768px) 33vw, (max-width:1024px) 50vw, 100vw"
+              className="object-cover rounded-sm saturate-150"
+            />
+          </motion.div>
+          <motion.div
+            className="relative w-[300px] h-[200px]"
+            variants={imageAnimateChild}
+          >
+            <Image
+              src="/img/GIIfwOhW0AAcKb2.png"
+              alt="image"
+              fill
+              sizes="(max-width:768px) 33vw, (max-width:1024px) 50vw, 100vw"
+              className="object-cover rounded-sm saturate-150"
+            />
+          </motion.div>
         </motion.div>
-        <motion.div
-          className="relative w-[300px] h-[200px]"
-          variants={imageAnimateChild}
-        >
-          <Image
-            src="/img/GDusokhWQAAVJOd.jpg"
-            alt="image"
-            fill
-            sizes="(max-width:768px) 33vw, (max-width:1024px) 50vw, 100vw"
-            className="object-cover rounded-sm saturate-150"
-          />
-        </motion.div>
-        <motion.div
-          className="relative w-[300px] h-[200px]"
-          variants={imageAnimateChild}
-        >
-          <Image
-            src="/img/GIIfwOhW0AAcKb2.png"
-            alt="image"
-            fill
-            sizes="(max-width:768px) 33vw, (max-width:1024px) 50vw, 100vw"
-            className="object-cover rounded-sm saturate-150"
-          />
-        </motion.div>
-      </motion.div>
-    </main>
+      </div> */}
+    </motion.main>
   );
 }
